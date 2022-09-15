c545d7c803ee52a6ede438b5a5b4579a605c5b6d
JellyWilliam <jellymelly@inbox.ru>
2022-09-15 15:43:51 +0300

    Окончательно исправил proxy

--- routes/hello_world/hello_world.js
+++ routes/hello_world/hello_world.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.all('/', (req, res) => {
+router.all('/hello_world', (req, res) => {
     return res.status(200).send('Hello world');
 });
 
--- routes/proxy/simple_proxy.js
+++ routes/proxy/simple_proxy.js
@@ -14,7 +14,7 @@ const logger = winston.createLogger({
     ]
 })
 
-router.all("/api/*", (req, res) => {
+router.all("/*", (req, res) => {
     const today = new Date();
     const YYYY = today.getFullYear();
     const MM = String(today.getMonth() + 1).padStart(2, '0');
@@ -23,7 +23,7 @@ router.all("/api/*", (req, res) => {
     const mm = String(today.getMinutes()).padStart(2, '0');
     const ss = String(today.getSeconds()).padStart(2, '0');
 
-    req.url = req.url.split('/').slice(2).join('/');
+    req.url = 'http://' + req.url.split('/').slice(2).join('/');
 
     logger.info("[" + YYYY + '-' + MM + '-' + DD + ' ' + hh + ':' + mm + ":" + ss + '] ' +
         req.socket.remoteAddress + " " + req.method + " " + req.url);
